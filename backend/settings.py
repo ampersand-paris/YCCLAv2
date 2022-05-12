cc4d6282b153d03de1ed592c55c7ade5f8cfa10f
ampersand-paris <ampersand.s.paris@gmail.com>
2022-05-11 22:06:34 -0400

    custom user and superuser

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = "accounts.CustomUser"
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.AllowAllUsersModelBackend',
+    'accounts.backends.CaseInsensitiveModelBackend'
+)
 # Application definition
 
 INSTALLED_APPS = [
