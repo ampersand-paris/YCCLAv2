48ecf65eb6227a4b7ee17b1116398ea2b444cada
ampersand-paris <ampersand.s.paris@gmail.com>
2022-05-12 10:09:16 -0400

    styles transferred

--- backend/urls.py
+++ backend/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from accounts.views import(
     register_view
@@ -29,3 +31,7 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
