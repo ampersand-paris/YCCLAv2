566a87afeb811138b7e9ce0fb0f819c58039e169
ampersand-paris <ampersand.s.paris@gmail.com>
2022-05-17 14:09:44 -0400

    heroku deploy

--- staticfiles/js/script.a24f122ef99a.js
+++
@@ -1,9 +0,0 @@
-const toggleTab = (id) => {
-    if (id === 'postcards'){
-        document.getElementById('postcards-tab').style.display='flex';
-        document.getElementById('posts-tab').style.display='none';
-    } else if (id === 'posts') {
-        document.getElementById('posts-tab').style.display='flex';
-        document.getElementById('postcards-tab').style.display='none';
-    }
-}
