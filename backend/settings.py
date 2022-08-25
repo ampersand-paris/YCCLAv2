f5361353b6df854455165cf2730df2bbe4176d63
ampersand-paris <ampersand.s.paris@gmail.com>
2022-08-25 13:31:16 -0400

    remove sentry

--- backend/settings.py
+++ backend/settings.py
@@ -199,21 +199,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import django_heroku  
 django_heroku.settings(locals())
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
+# import sentry_sdk
+# from sentry_sdk.integrations.django import DjangoIntegration
 
-sentry_sdk.init(
-    dsn="https://982487abd17f4a6c8f9d70fe3c0de60d@o1377629.ingest.sentry.io/6688515",
-    integrations=[
-        DjangoIntegration(),
-    ],
+# sentry_sdk.init(
+#     dsn="https://982487abd17f4a6c8f9d70fe3c0de60d@o1377629.ingest.sentry.io/6688515",
+#     integrations=[
+#         DjangoIntegration(),
+#     ],
 
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
+#     # Set traces_sample_rate to 1.0 to capture 100%
+#     # of transactions for performance monitoring.
+#     # We recommend adjusting this value in production.
+#     traces_sample_rate=1.0,
 
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
+#     # If you wish to associate users to errors (assuming you are using
+#     # django.contrib.auth) you may enable sending PII data.
+#     send_default_pii=True
+# )
